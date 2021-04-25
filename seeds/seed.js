6dced0f61ac8017cf7b724231dcb0b01b18e0233
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-04-25 16:09:20 -0700

    updates to seed files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,14 +1,29 @@
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
 const commentSeeds = require("./commentSeeds");
 const postSeeds = require("./postSeeds");
 const userSeeds = require('./userSeeds');
 
+
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-  await postSeeds();
-  await commentSeeds();
-  await userSeeds();
-  
+  // await postSeeds();
+  // await commentSeeds();
+  // await userSeeds();
+  const users = await User.bulkCreate(userSeeds, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const posts = await Post.bulkCreate(postSeeds, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const comments = await Comment.bulkCreate(commentSeeds, {
+    individualHooks: true,
+    returning: true,
+  });
   process.exit(0);
 };
 
