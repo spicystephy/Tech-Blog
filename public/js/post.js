5387a075edda9cc96629c1348a7995ca8dcb1ac0
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-10 22:20:17 -0700

    added posts logic

--- public/js/post.js
+++ public/js/post.js
@@ -1,24 +1,25 @@
-async function postFormHandler(event){
-    event.preventDefault();
+async function postFormHandler(event) {
+  event.preventDefault();
 
-    if (post_text) {
-        const response = await fetch('/api/:id', {
-            method: 'GET',
-            body: JSON.stringify({
-                title,
-                post_text
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+  if (post_text) {
+    const response = await fetch("/api/posts/:id", {
+      method: "GET",
+      body: JSON.stringify({
+        title,
+        post_text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        if (response.ok) {
-            location.href= `/api/search/${bathroom_id}`;
-
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      location.href = "/api/posts/:id";
+    } else {
+      alert(response.statusText);
     }
+  }
 }
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document
+  .querySelector(".comment-form")
+  .addEventListener("submit", commentFormHandler);
