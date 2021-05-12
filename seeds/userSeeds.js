1aec11b0d62d0622ecba2cc3ec7ab41dfe298893
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-12 13:01:32 -0700

    deleted user_id

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -5,25 +5,21 @@ const userdata = [
     username: "Maeby",
     email: "maeby@email.com",
     password: "pw12345",
-    user_id: "1",
   },
   {
     username: "Tobias",
     email: "tobias@email.com",
     password: "pw12345",
-    user_id: "2",
   },
   {
     username: "Lindsay",
     email: "lindsay@email.com",
     password: "pw12345",
-    user_id: "3",
   },
   {
     username: "Lucille",
     email: "lucille@email.com",
     password: "pw12345",
-    user_id: "4",
   },
 ];
 const users = () => User.bulkCreate(userdata);
