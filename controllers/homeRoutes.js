d7dff0504e50c5a995df4407e4fd9973beb8ccbd
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-11 18:15:07 -0700

    fixed routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const { Post, Comment, User } = require("../models");
 const sequelize = require("../config/connection");
 const withAuth = require("../utils/auth");
 
-//homepage with user's posts
+//homepage with users' posts
 router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render("homepage", {
       posts,
-      logged_in: req.session.logged_in,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
