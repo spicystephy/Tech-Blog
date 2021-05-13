d794f6b74fe3f3a6c6b91de55eb9f9b9c679c939
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-13 16:39:46 -0700

    add to get route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,18 +7,18 @@ const withAuth = require("../utils/auth");
 router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
-      // attributes: ["title", "post_text", "user_id"],
+      attributes: ["title", "post_text", "date_created", "id", "user_id"],
       include: [
         {
           model: User,
-          // attributes: ["username"],
+          attributes: ["username"],
         },
       ],
     });
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render("homepage", {
       posts,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
